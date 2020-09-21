96e9b5fb5e472a4f974ce132c581d668c8047f03
olliy1or <55839810+olliy1or@users.noreply.github.com>
2020-09-21 21:33:18 +0500

    Use Embed for !help command
    Co-authored-by: rubenwardy <rw@rubenwardy.com>
    Co-authored-by: LoneWolfHT <lonewolf04361@gmail.com>

--- index.js
+++ index.js
@@ -186,7 +186,16 @@ client.on("message", message => {
 			message.channel.send(formatLeaderboard(orderedList));
 		}
 	} else if (command == "help") {
-		message.channel.send("Commands: `!rank`, `!oldrank`, `!oldleaders`, `!help`");
+		const helpEmbed = new Discord.MessageEmbed()
+			.setTitle("Commands")
+			.setColor("#0000E5")
+			.addField(prefix + "rank", "Shows ingame rankings", false)
+			.addField(prefix + "oldrank", "Shows old rankings", false)
+			.addField(prefix + "leaders", "Shows the top 60 leaderboard or links to the dedicated channel for it", false)
+			.addField(prefix + "oldleaders", "Shows old top 60 leaderboard", false)
+			.addField(prefix + "help", "Shows the available commands", false)
+
+		return message.channel.send(helpEmbed);
 	}
 });
 
