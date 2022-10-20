6143fee6abb509347a05e5047ff66b1836b19f76
AFCMS <afcm.contact@gmail.com>
2022-10-20 16:24:57 +0200

    Add slash commands (Fix #13) (#14)

--- README.md
+++ README.md
@@ -1,28 +1,28 @@
 # CTF Discord Bot
 
-Requires Node v14.
+Requires Node v16.9.0
 
 License: MIT
 
 ## Features
 
-* Show rank stats embed for a player.
-  * `!rank` - Stats for player with the Discord user's username or nickname.
-  * `!rank username` - Stats for player `username`.
-  * `!help` - Help.
-* Posts leaderboard to a channel, editing existing messages.
+- Show rank stats embed for a player.
+  - `/rank` - Stats for player with the Discord user's username or nickname.
+  - `/rank username` - Stats for player `username`.
+  - `/x` - Send message on the staff channel (See [this](https://github.com/MT-CTF/servermods/blob/master/server_chat/init.lua#L77)).
+  - `/mute user` - Mute Discord member.
+  - `/unmute user` - Unmute Discord member.
+  - `/leaders` - Redirect to the leaderboard channel.
+- Posts leaderboard to a channel, editing existing messages.
 
 ## Usage
 
-* Create Discord bot and application using the [Discord Developer Portal](https://discord.com/developers/applications/),
+- Create Discord bot and application using the [Discord Developer Portal](https://discord.com/developers/applications/),
   as shown in [this guide](https://discordjs.guide/preparations/setting-up-a-bot-application.html#creating-your-bot).
-* Invite the Discord bot to the desired server, as shown in
+- Invite the Discord bot to the desired server, as shown in
   [this guide](https://discordjs.guide/preparations/adding-your-bot-to-servers.html#bot-invite-links).
-* Install dependencies: `npm install`.
-* Run with environment variables: `RANKINGS_CHANNEL=id TOKEN=discord_token npm start`
-  * `RANKINGS_CHANNEL` - Channel ID for the leaderboard.
-  * `TOKEN` - Discord bot token, get from bot page in Discord Developer Portal.
-
-## Other
-
-You can grab messages sent with !x via Minetest. See https://github.com/MT-CTF/servermods/blob/master/server_chat/init.lua#L77 for an example
+- Install dependencies: `npm install`.
+- Run with environment variables: `GUILD_ID=guild_id RANKINGS_CHANNEL=id TOKEN=discord_token npm start`
+  - `GUILD_ID` - Guild ID used for slash commands.
+  - `RANKINGS_CHANNEL` - Channel ID for the leaderboard.
+  - `TOKEN` - Discord bot token, get from bot page in Discord Developer Portal.
--- index.js
+++ index.js
@@ -1,270 +1,474 @@
-const Discord = require("discord.js");
-const discordClient = new Discord.Client();
-const redisClient = require("redis").createClient();
-const rankingsChannel = process.env.RANKINGS_CHANNEL;
-const prefix = "!";
+import Discord from "discord.js"
+import redis from "redis"
+import http from "http"
+import "dotenv/config"
+
+const discordClient = new Discord.Client({
+	intents: [
+		Discord.GatewayIntentBits.Guilds,
+		Discord.GatewayIntentBits.GuildMessages,
+		Discord.GatewayIntentBits.GuildIntegrations,
+	],
+})
+
+const redisClient = redis.createClient()
+
+const guildId = process.env.GUILD_ID
+const rankingsChannel = process.env.RANKINGS_CHANNEL
+const token = process.env.TOKEN
 
 let staffMessages = []
-let statsMap = null;
+let statsMap = null
+let statsPlayers = null
 
 function formatLeaderboard(list, mode) {
-	const rankingsEmbed = new Discord.MessageEmbed()
+	const rankingsEmbed = new Discord.EmbedBuilder()
 		.setColor("#0099ff")
-		.setTitle("CTF Rankings for mode " + mode);
+		.setTitle("CTF Rankings for mode " + mode)
 
-	const max = Math.min(60, list.length);
+	const max = Math.min(60, list.length)
 	for (var i = 0; i < max; i += 20) {
-		const from = i;
+		const from = i
 		const to = i + 20
 
-		const newContent = list.slice(from, to)
-			.map(stats => {
-				let kd = stats.kills || 0;
-				kd /= stats.deaths || 1;
+		const newContent = list
+			.slice(from, to)
+			.map((stats) => {
+				let kd = stats.kills || 0
+				kd /= stats.deaths || 1
 
 				stats.name = stats.name.replace("_", "\\_")
 
-				return `**${stats.place}. ${stats.name}**\nK/D: ${kd.toFixed(1)} - Score: *${Math.round(stats.score)}*`;
+				return `**${stats.place}. ${stats.name}**\nK/D: ${kd.toFixed(
+					1
+				)} - Score: *${Math.round(stats.score)}*`
 			})
-			.join("\n");
+			.join("\n")
 
-		rankingsEmbed.addField(`__Top ${from+1} - ${to}__`, newContent, true)
+		rankingsEmbed.addFields([
+			{ name: `__Top ${from + 1} - ${to}__`, value: newContent },
+		])
 	}
 
-	return rankingsEmbed;
+	return rankingsEmbed
 }
 
 async function updateRankingsChannel(statsList) {
 	if (!rankingsChannel) {
-		return;
+		return
 	}
 
-	const channel = discordClient.channels.cache.get(rankingsChannel);
+	const channel = discordClient.channels.cache.get(rankingsChannel)
 	if (!channel) {
-		return;
+		return
 	}
 
-	let rankings = [];
+	let rankings = []
 	for (const mode of [...statsList.keys()].sort()) {
-		rankings.push(formatLeaderboard(statsList.get(mode), mode));
+		rankings.push(formatLeaderboard(statsList.get(mode), mode))
 	}
 
-	const messages = await channel.messages.fetch({ limit: rankings.length });
+	const messages = await channel.messages.fetch({ limit: rankings.length })
 	if (messages.size < rankings.length) {
 		for (var i = 0; i < rankings.length; ++i) {
-			await channel.send(rankings[i]);
+			await channel.send({ embeds: [rankings[i]], ephemeral: false })
 		}
 	} else {
 		let it = messages.values()
 		for (var i = 0; i < rankings.length; ++i) {
-			await it.next().value.edit(rankings[rankings.length - i - 1]);
+			await it.next().value.edit({
+				embeds: [rankings[rankings.length - i - 1]],
+				ephemeral: false,
+			})
 		}
 	}
 }
 
 async function updateRankings() {
-	let statsList = new Map();
+	let statsList = new Map()
+	statsPlayers = []
 	for (const key of await redisClient.keys("ctf_mode_*")) {
-		const [rawMode, name] = key.split("|", 2);
-		const mode = rawMode.substring(9).split("_").map((word) => {
-			return word[0].toUpperCase() + word.substring(1);
-		}).join(" ")
+		const [rawMode, name] = key.split("|", 2)
+		const mode = rawMode
+			.substring(9)
+			.split("_")
+			.map((word) => {
+				return word[0].toUpperCase() + word.substring(1)
+			})
+			.join(" ")
 
-		let stats = JSON.parse(await redisClient.get(key));
+		let stats = JSON.parse(await redisClient.get(key))
 
 		if (stats) {
-			stats["name"] = name;
-			stats.score = stats.score || 0;
+			stats["name"] = name
+			stats.score = stats.score || 0
 
 			if (statsList.get(mode) === undefined) {
-				statsList.set(mode, []);
+				statsList.set(mode, [])
 			}
 
-			statsList.get(mode).push(stats);
+			statsList.get(mode).push(stats)
 		}
+		statsPlayers.push(name)
 	}
 
-	statsMap = new Map();
+	statsMap = new Map()
 
 	for (const [mode, stats] of statsList) {
-		stats.sort((a, b) => b.score - a.score);
+		stats.sort((a, b) => b.score - a.score)
 		stats.forEach((stat, i) => {
-			stat.place = i + 1;
-		});
+			stat.place = i + 1
+		})
 
-		statsMap.set(mode, new Map());
+		statsMap.set(mode, new Map())
 		for (const stat of stats) {
-			statsMap.get(mode).set(stat.name.toLowerCase(), stat);
+			statsMap.get(mode).set(stat.name.toLowerCase(), stat)
 		}
 	}
 
 	await updateRankingsChannel(statsList)
+	statsPlayers.sort()
 }
 
 function ordinalSuffixOf(i) {
-	var j = i % 10, k = i % 100;
+	var j = i % 10,
+		k = i % 100
 	if (j == 1 && k != 11) {
-		return i + "st";
+		return i + "st"
 	}
 	if (j == 2 && k != 12) {
-		return i + "nd";
+		return i + "nd"
 	}
 	if (j == 3 && k != 13) {
-		return i + "rd";
+		return i + "rd"
 	}
-	return i + "th";
+	return i + "th"
 }
 
 function normValue(v) {
-	return Math.round(v || 0);
+	return Math.round(v || 0)
 }
 
 function formatRanking(stats, mode) {
-	let kd = stats.kills || 0;
-	kd /= stats.deaths || 1;
+	let kd = stats.kills || 0
+	kd /= stats.deaths || 1
 
 	const fields = [
-		{ name: "Kills", value: normValue(stats.kills), inline: true },
-		{ name: "Deaths", value: normValue(stats.deaths), inline: true },
+		{ name: "Kills", value: normValue(stats.kills).toString(), inline: true },
+		{ name: "Deaths", value: normValue(stats.deaths).toString(), inline: true },
 		{ name: "K/D", value: kd.toFixed(1), inline: true },
-		{ name: "Bounty kills", value: normValue(stats.bounty_kills), inline: true },
-		{ name: "Captures", value: normValue(stats.flag_captures), inline: true },
-		{ name: "HP healed", value: normValue(stats.hp_healed), inline: true },
+		{
+			name: "Bounty kills",
+			value: normValue(stats.bounty_kills).toString(),
+			inline: true,
+		},
+		{
+			name: "Captures",
+			value: normValue(stats.flag_captures).toString(),
+			inline: true,
+		},
+		{
+			name: "HP healed",
+			value: normValue(stats.hp_healed).toString(),
+			inline: true,
+		},
 	]
 
-	return new Discord.MessageEmbed()
+	return new Discord.EmbedBuilder()
 		.setColor("#0099ff")
 		.setTitle(`${stats.name}, ${ordinalSuffixOf(stats.place)}, ${mode}`)
-		.setDescription(`${stats.name} is in ${ordinalSuffixOf(stats.place)} place, with ${Math.round(stats.score)} score, ${mode} mode.`)
-		.addFields(fields)
-		.setTimestamp();
+		.setDescription(
+			`${stats.name} is in ${ordinalSuffixOf(
+				stats.place
+			)} place, with ${Math.round(stats.score)} score, ${mode} mode.`
+		)
+		.setFields(fields)
+		.setTimestamp()
 }
 
-function handleRankRequest(message, command, args) {
-	if (!statsMap) {
-		message.channel.send("Please wait, stats are still loading...");
-		return;
-	}
+const error_embed_admin_mute = new Discord.EmbedBuilder()
+	.setColor(Discord.Colors.Red)
+	.setDescription("The user you are trying to mute is a staff member!")
 
-	const username = message.author.username.trim();
-	const nickname = message.member.nickname ? message.member.nickname.trim() : username;
-	const name = args.length > 0 ? args[0].trim() : nickname;
+const error_embed_bot_mute = new Discord.EmbedBuilder()
+	.setColor(Discord.Colors.Red)
+	.setDescription("No. You can't do that.")
 
-	let playerStats = [];
+const error_embed_stats_unavaillable = new Discord.EmbedBuilder()
+	.setColor(Discord.Colors.Red)
+	.setDescription("Please wait, stats are still loading...")
 
-	for (const mode of [...statsMap.keys()].sort()) {
-		let stat = statsMap.get(mode).get(name.toLowerCase());
-		if (!stat && args.length == 0) {
-			stat = statsMap.get(mode).get(username.toLowerCase());
-		}
-		if (stat) {
-			playerStats.push([stat, mode]);
-		}
-	}
-
-	if (playerStats.length > 0) {
-		for (const [stat, mode] of playerStats) {
-			message.channel.send(formatRanking(stat, mode));
-		}
-
-		if (args.length > 0 && (name.toLowerCase() == nickname.toLowerCase() || name.toLowerCase() == username.toLowerCase())) {
-			message.channel.send(`_pst: you can just use \`!${command}\`_`);
-		}
-	} else {
-		if (args.length > 0) {
-			message.channel.send(`Unable to find user ${name}`);
-		} else if (username.toLowerCase() != nickname.toLowerCase()) {
-			message.channel.send(`Unable to find ${nickname} or ${username}, please provide username explicitly like so: \`!rank username\``);
-		} else {
-			message.channel.send(`Unable to find ${nickname}, please provide username explicitly like so: \`!rank username\``);
-		}
-	}
+let embed_leaders = new Discord.EmbedBuilder().setColor(Discord.Colors.Blue)
+if (rankingsChannel) {
+	embed_leaders = embed_leaders.setDescription(`Checkout <#${rankingsChannel}>`)
+} else {
+	embed_leaders = embed_leaders.setDescription("There is no rankings channel")
 }
+
+// commands definition
+
+const commands = []
+
+//fixme
+commands.push(
+	new Discord.SlashCommandBuilder()
+		.setName("rank")
+		.setDescription("Shows ingame rankings")
+		.addStringOption((option) =>
+			option
+				.setName("player")
+				.setDescription("The player")
+				.setAutocomplete(true)
+		)
+)
+
+commands.push(
+	new Discord.SlashCommandBuilder()
+		.setName("x")
+		.setDescription("Send messages on staff channel")
+		.setDefaultMemberPermissions(Discord.PermissionsBitField.Flags.KickMembers)
+		.addStringOption((option) =>
+			option
+				.setName("message")
+				.setDescription("Enter message")
+				.setRequired(true)
+		)
+)
+
+commands.push(
+	new Discord.SlashCommandBuilder()
+		.setName("leaders")
+		.setDescription(
+			"Shows the top 60 leaderboard or links to the dedicated channel for it"
+		)
+)
+
+commands.push(
+	new Discord.SlashCommandBuilder()
+		.setName("mute")
+		.setDescription("Mutes a user")
+		.setDefaultMemberPermissions(Discord.PermissionsBitField.Flags.KickMembers)
+		.addUserOption((option) =>
+			option.setName("user").setDescription("User to mute").setRequired(true)
+		)
+)
+
+commands.push(
+	new Discord.SlashCommandBuilder()
+		.setName("unmute")
+		.setDescription("Unmutes a user")
+		.setDefaultMemberPermissions(Discord.PermissionsBitField.Flags.KickMembers)
+		.addUserOption((option) =>
+			option.setName("user").setDescription("User to unmute").setRequired(true)
+		)
+)
 
 discordClient.on("ready", () => {
-	console.log(`Logged in as ${discordClient.user.tag}!`);
-});
+	console.log(`Logged in as ${discordClient.user.tag}`)
 
-discordClient.on("message", message => {
-	if (message.content[0] != prefix) {
-		return;
-	}
+	const guild = discordClient.guilds.resolve(guildId)
 
-	const args = message.content.slice(prefix.length).trim().split(" ");
-	const command = args.shift().toLowerCase();
+	//push commands to server
+	guild.commands.set(commands).catch(console.error)
+})
 
-	if (command == "rank" || command == "r" || command == "rankings") {
-		handleRankRequest(message, command, args);
-	} else if (command == "leaders") {
-		if (rankingsChannel) {
-			message.channel.send(`<#${rankingsChannel}>`);
+discordClient.on("interactionCreate", async (interaction) => {
+	if (
+		interaction.type === Discord.InteractionType.ApplicationCommandAutocomplete
+	) {
+		if (interaction.commandName === "rank") {
+			if (!statsPlayers) {
+				interaction.respond([])
+				return
+			}
+			const focusedValue = interaction.options.getFocused().toLowerCase()
+			const filtered = statsPlayers.filter((p) =>
+				p.toLowerCase().startsWith(focusedValue)
+			)
+			await interaction.respond(
+				filtered.slice(0, 10).map((p) => ({ name: p, value: p }))
+			)
 		}
-	} else if (command == "help") {
-		const helpEmbed = new Discord.MessageEmbed()
-			.setTitle("Commands")
-			.setColor("#0000E5")
-			.addField(prefix + "rank", "Shows ingame rankings", false)
-			.addField(prefix + "leaders", "Shows the top 60 leaderboard or links to the dedicated channel for it", false)
-			.addField(prefix + "help", "Shows the available commands", false)
-			.addField(prefix + "mute <@username>", "Mutes a user", false)
-			.addField(prefix + "unmute <@username>", "Unmutes a user", false)
+	} else if (interaction.isCommand()) {
+		const { commandName, member, memberPermissions, options } = interaction
 
-		return message.channel.send(helpEmbed);
-	} else if (command == "mute") {
-		if (!message.member.hasPermission("KICK_MEMBERS"))
-			return message.reply("You dont have the permission to run this command");
+		//handle commands
+		if (commandName === "rank") {
+			if (!statsMap) {
+				interaction.reply({
+					embeds: [error_embed_stats_unavaillable],
+					ephemeral: true,
+				})
+			}
 
-		let muteuser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-		if (!muteuser)
-			return message.reply("Couldn't find user");
+			const option_player = options.getString("player")
+			if (option_player) {
+				let playerStats = []
 
-		if (muteuser.hasPermission("KICK_MEMBERS"))
-			return message.reply("Can't mute them, the user you are trying to mute is a staff member!");
+				for (const mode of [...statsMap.keys()].sort()) {
+					let stat = statsMap.get(mode).get(option_player.toLowerCase())
+					if (stat) {
+						playerStats.push([stat, mode])
+					}
+				}
 
-		let muterole = message.guild.roles.cache.find(role => role.name === "Muterated");
-		muteuser.roles.add(muterole.id);
+				if (playerStats.length > 0) {
+					let embeds = []
+					for (const [stat, mode] of playerStats) {
+						embeds.push(formatRanking(stat, mode))
+						interaction.reply({ embeds: embeds, ephemeral: false })
+					}
+				} else {
+					interaction.reply({
+						embeds: [
+							new Discord.EmbedBuilder()
+								.setColor(Discord.Colors.Red)
+								.setDescription(`Unable to find ${option_player}.`),
+						],
+						ephemeral: false,
+					})
+				}
+			} else {
+				const username = member.user.username.trim()
+				const nickname = member.nickname
+				const name = nickname ? nickname : username
 
-		message.reply(`<@${muteuser.id}> has been muted`);
-	} else if (command == "unmute") {
-		if (!message.member.hasPermission("KICK_MEMBERS"))
-			return message.reply("You dont have the permission to run this command");
+				let playerStats = []
 
-		let unmuteuser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-		if (!unmuteuser)
-			return message.reply("Couldn't find user");
+				for (const mode of [...statsMap.keys()].sort()) {
+					let stat = statsMap.get(mode).get(name.toLowerCase())
+					if (!stat) {
+						stat = statsMap.get(mode).get(username.toLowerCase())
+					}
+					if (stat) {
+						playerStats.push([stat, mode])
+					}
+				}
 
-		let muterole = message.guild.roles.cache.find(role => role.name === "Muterated");
-		(unmuteuser.roles.remove(muterole.id));
+				if (playerStats.length > 0) {
+					let embeds = []
+					for (const [stat, mode] of playerStats) {
+						embeds.push(formatRanking(stat, mode))
+						interaction.reply({ embeds: embeds, ephemeral: false })
+					}
+				} else {
+					if (username.toLowerCase() != name.toLowerCase()) {
+						interaction.reply({
+							embeds: [
+								new Discord.EmbedBuilder()
+									.setColor(Discord.Colors.Red)
+									.setDescription(
+										`Unable to find ${nickname} or ${username}, please provide username explicitly.`
+									),
+							],
+							ephemeral: false,
+						})
+					} else {
+						interaction.reply({
+							embeds: [
+								new Discord.EmbedBuilder()
+									.setColor(Discord.Colors.Red)
+									.setDescription(
+										`Unable to find ${nickname}, please provide username explicitly.`
+									),
+							],
+							ephemeral: false,
+						})
+					}
+				}
+			}
+		} else if (commandName === "x") {
+			staffMessages.push(
+				`<${member.user.username}@Discord> ${options.getString("message")}`
+			)
 
-		message.reply(`<@${unmuteuser.id}> has been unmuted`);
-	} else if (command == "x") {
-		if (!message.member.hasPermission("KICK_MEMBERS"))
-			return message.reply("You dont have the permission to run this command");
+			interaction.reply({
+				embeds: [
+					new Discord.EmbedBuilder()
+						.setColor(Discord.Colors.Blue)
+						.setDescription(
+							`**${member.user.username}**: ${options.getString("message")}`
+						),
+				],
+				ephemeral: false,
+			})
+		} else if (commandName === "leaders") {
+			await interaction.reply({ embeds: [embed_leaders], ephemeral: true })
+		} else if (commandName === "mute") {
+			const muted_member = options.getMember("user")
 
-		staffMessages.push(`<${message.member.user.username}@Discord> ${message.content.substring(2).trim()}`);
+			let muterole = interaction.guild.roles.cache.find(
+				(role) => role.name === "Muterated"
+			)
 
-		message.react('☑️');
+			if (discordClient.user.id === muted_member.user.id) {
+				interaction.reply({
+					embeds: [error_embed_bot_mute],
+					ephemeral: true,
+				})
+			} else if (
+				muted_member.permissions.has(
+					Discord.PermissionsBitField.Flags.KickMembers
+				)
+			) {
+				interaction.reply({
+					embeds: [error_embed_admin_mute],
+					ephemeral: true,
+				})
+			} else {
+				await muted_member.roles.add(muterole.id)
+				interaction.reply({
+					embeds: [
+						new Discord.EmbedBuilder()
+							.setColor(Discord.Colors.Blue)
+							.setDescription(
+								`**${muted_member.user.username}** has been muted.`
+							),
+					],
+					ephemeral: false,
+				})
+			}
+		} else if (commandName === "unmute") {
+			const muted_member = options.getMember("user")
+
+			let muterole = interaction.guild.roles.cache.find(
+				(role) => role.name === "Muterated"
+			)
+
+			await muted_member.roles.remove(muterole.id)
+			interaction.reply({
+				embeds: [
+					new Discord.EmbedBuilder()
+						.setColor(Discord.Colors.Blue)
+						.setDescription(
+							`**${muted_member.user.username}** has been unmuted.`
+						),
+				],
+				ephemeral: false,
+			})
+		}
 	}
-});
+})
 
 async function main() {
-	await discordClient.login(process.env.TOKEN);
-	await redisClient.connect();
+	await discordClient.login(token)
+	await redisClient.connect()
 
-	await updateRankings();
-	setInterval(updateRankings, 60000);
+	await updateRankings()
+	setInterval(updateRankings, 6000)
 
-	var http = require('http');
+	// prettier-ignore
 	http.createServer(function (req, res) {
-		if (req.method == "GET" && staffMessages.length > 0) {
-			res.writeHead(200, {'Content-Type': 'text/plain'});
-			console.log("Relaying staff messages: " + staffMessages.join("-|-"));
-			res.write(JSON.stringify(staffMessages));
-			staffMessages = [];
-		}
+			if (req.method == "GET" && staffMessages.length > 0) {
+				res.writeHead(200, { "Content-Type": "text/plain" })
+				console.log("Relaying staff messages: " + staffMessages.join("-|-"))
+				res.write(JSON.stringify(staffMessages))
+				staffMessages = []
+			}
 
-		res.writeHead(200);
-		res.end();
-	}).listen(31337, '127.0.0.1');
+			res.writeHead(200)
+			res.end()
+		})
+		.listen(31337, "127.0.0.1")
 }
 main()
